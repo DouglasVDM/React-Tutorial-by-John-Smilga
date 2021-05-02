3e61cbcbb9ab1f901cc0054af66a3d6e734b57c3
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-05-02 17:44:07 +0200

    add CSS inline
    *Adding CSS inline is stronger than the external CSS sheet*
    It will overwrite the styles on the external CSS sheet only for styles mentioned inline.

--- src/index.js
+++ src/index.js
@@ -46,6 +46,6 @@ function Book() {
 }
 const Image = () => <img src="https://yt3.ggpht.com/ytc/AAUvwniaHN7MZyFEiNvdHuKMzIWnDF604Z2--O4GCMq-FA=s48-c-k-c0x00ffffff-no-rj" alt="" />;
 const Name = () => <h1>React Tutorial - Fundamentals, Hooks, Context API, React Router, Custom Hooks</h1>;
-const Author = () => <h4>by John Smilga</h4>
+const Author = () => <h4 style={{color:"#617d98", fontSize:"0.75rem", marginTop:"0.25rem"}}>by John Smilga</h4>
 
 ReactDom.render(<BookList />, document.getElementById("root"));
