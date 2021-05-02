a31cea04468a60b8df42a699c6b90a83d71dd6fc
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-05-02 19:49:23 +0200

    add properties
    Working with props i.e. properties.
    Setup 2 books with different values for the same props
    Adding props as a perameter to Book component
    Pass in the props to the elements in Book component
    Access the values to the Book List component.

--- src/index.js
+++ src/index.js
@@ -25,21 +25,42 @@ const FirstComponent = () => <h4>This is Douglas and this is my first component.
 
 ReactDom.render(<Greeting />, document.getElementById("root"));
 
+ const firstBook = {
+  img: "https://yt3.ggpht.com/ytc/AAUvwniaHN7MZyFEiNvdHuKMzIWnDF604Z2--O4GCMq-FA=s48-c-k-c0x00ffffff-no-rj",
+  title: "React Tutorial - Fundamentals, Hooks, Context API, React Router, Custom Hooks",
+  author: "by John Smilga",
+}
+
+const secondBook = {
+  img: "chrome-extension://pachckjkecffpdphbpmfolblodfkgbhl/images/vidiq_playplus.png",
+  title: "VidIQ Icon",
+  author: "by VidIQ",
+}
+
 function BookList() {
   return <section className="booklist">
-    <Book />
+    <Book
+      img={firstBook.img}
+      title={firstBook.title}
+      author={firstBook.author}      
+    />
+
+    <Book
+      img={secondBook.img}
+      title={secondBook.title}
+      author={secondBook.author}      
+    />
   </section>
 }
-const author = "by John Smilga";
-const imgSrc = "https://yt3.ggpht.com/ytc/AAUvwniaHN7MZyFEiNvdHuKMzIWnDF604Z2--O4GCMq-FA=s48-c-k-c0x00ffffff-no-rj";
 
-const Book = () => {
-  const title = "React Tutorial - Fundamentals, Hooks, Context API, React Router, Custom Hooks";
-  return <article className="book" style={{ border: "solid 1px" }}>
-    <img src={ imgSrc } alt="" />
-    <h1>{title}</h1>
-    <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>{ author }</h4>
-  </article>
+const Book = (props) => {
+  return (
+    <article className="book" style={{ border: "solid 1px" }}>
+    <img src={props.img} alt="" />
+    <h1>{props.title}</h1>
+    <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>{props.author}</h4>   
+    </article>
+  )
 }
 
 ReactDom.render(<BookList />, document.getElementById("root"));
