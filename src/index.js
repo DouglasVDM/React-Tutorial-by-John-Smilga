d7f005852d6dcb387ffb82683ffba99858bc2a16
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-05-05 22:21:50 +0200

    Click event and mouseOver event
    1st looking for attribute: onClick, onMouseOver, etc.
    implicid return: console.log
    Reference method with clickHandler function
    When there is a parameter in the clickHandler function, it must be wrap in an arrow function. Otherwise it will be invoked when the page is loaded.

--- src/index.js
+++ src/index.js
@@ -58,16 +58,23 @@ const Book = (props) => {
   const { img, title, author } = props; //  Object Destructuring.
   //  Attribute, eventHandler
   //  onClick, onMouseOver
-  const clickHandler = () => {
-    alert("Hello World");
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    // alert("Hello World");
+  }
+
+  const complexExample = (author) => {
+    console.log(author)
   }
   return (
     <article className="book" style={{ border: "solid 1px" }}>
     <img src={img} alt="" />
-    <h1 onClick={()=>console.log(`Title:${title}`)}>{title}</h1>
-      <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>{author}
-      </h4>
-      <button type="button" onClick={clickHandler}>Reference Example</button>
+    <h1 onClick={() => console.log(title)}>{title}</h1>
+    <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>{author}
+    </h4>
+    <button type="button" onClick={clickHandler()}>Reference Example</button>
+    <button type="button" onClick={() => complexExample(author)}>More Complex Example</button>
     </article>
   )
 }
